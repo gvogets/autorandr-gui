0ea565cdc5cadc8d42b7d3acfd788b2c2762af8f
Georg Vogetseder <Georg Vogetseder externer.dl.vogetseder@muenchen.de>
2012-01-19 17:45:45 +0100

    Extended Toolbar, Better Listings, Add support for Standard Profiles

--- gui/gui.py
+++ gui/gui.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# coding=utf-8
 
 import wx
 
@@ -20,20 +21,25 @@ class ArFrame(wx.Frame):
   def __toolbar(self):
     """ Build our toolbar """
     self.toolbar = self.CreateToolBar(style=wx.TB_HORZ_TEXT)
-    saveTool = self.toolbar.AddLabelTool(wx.ID_SAVE, 'Speichern', \
+    saveTool = self.toolbar.AddLabelTool(wx.ID_SAVE, u'Speichern', \
       self.getbitmap(wx.ART_FILE_SAVE_AS, wx.ART_TOOLBAR))
-    standardTool = self.toolbar.AddLabelTool(wx.ID_ANY, 'Standard festlegen', \
+    deleteTool = self.toolbar.AddLabelTool(wx.ID_DELETE, u'Löschen', \
+      self.getbitmap(wx.ART_DELETE, wx.ART_TOOLBAR))
+    self.toolbar.AddSeparator()
+    standardTool = self.toolbar.AddLabelTool(wx.ID_ANY, u'Standard festlegen', \
       self.getbitmap(wx.ART_TICK_MARK, wx.ART_TOOLBAR))
-    quitTool = self.toolbar.AddLabelTool(wx.ID_EXIT, 'Beenden', \
+    self.toolbar.AddSeparator()
+    quitTool = self.toolbar.AddLabelTool(wx.ID_EXIT, u'Beenden', \
       self.getbitmap(wx.ART_QUIT, wx.ART_TOOLBAR))
     self.toolbar.Realize()
-
     self.Bind(wx.EVT_TOOL, self.OnQuit, quitTool)
+    self.Bind(wx.EVT_TOOL, self.OnDelete, deleteTool)
     self.Bind(wx.EVT_TOOL, self.OnStandard, standardTool)
     self.Bind(wx.EVT_TOOL, self.OnSave, saveTool)
 
   def __vertbox(self):
-    vbox = wx.BoxSizer(wx.VERTICAL)
+    sb = wx.StaticBox(self, label="Gespeicherte Profile")
+    vbox = wx.StaticBoxSizer(sb, wx.VERTICAL)
     self.SetSizerAndFit(vbox)
     return vbox
 
@@ -41,7 +47,7 @@ class ArFrame(wx.Frame):
       dimensions='Abmessungen nicht bekannt', makeline=True, status=None):
     hbox = wx.BoxSizer(wx.HORIZONTAL)
     """ Define the left Text-Box """
-    txtwidth = 250
+    txtwidth = 300
     stname = wx.StaticText(self, label=name)
     stname.SetFont(self.font)
     stname.Wrap(txtwidth)
@@ -62,11 +68,15 @@ class ArFrame(wx.Frame):
       if "standard" in status:
         statustxt = "Standardprofil"
       if "detected" in status:
-        statustxt = "Erkanntes Profil"
+        statustxt = "erkanntes Profil"
       if ("detected" in status) and ("standard" in status) :
-        statustxt = "Standardprofil\nErkanntes Profil"
+        statustxt = "Standardprofil\nerkanntes Profil"
     btntxt = wx.StaticText(self, label=statustxt)
-    btnapply = wx.Button(self, id=wx.ID_APPLY, name=name)
+    if status != None and "active" in status:
+      btnapply = wx.Button(self, id=wx.ID_REFRESH, name=name)
+      stname.SetLabel(name + " (aktiv)")
+    else:
+      btnapply = wx.Button(self, id=wx.ID_APPLY, name=name)
     self.Bind(wx.EVT_BUTTON, self.OnApply)
     btnbox.Add(btntxt, flag=wx.ALL|wx.EXPAND|wx.ALIGN_CENTER, border=1)
     btnbox.Add(btnapply, flag=wx.ALL, border=1)
@@ -80,15 +90,35 @@ class ArFrame(wx.Frame):
     if makeline:
       self.vertbox.Add(wx.StaticLine(self), \
         flag=wx.GROW|wx.ALIGN_CENTER_VERTICAL|wx.ALL, border=5)
+    self.vertbox.Layout()
 
   def OnQuit(self, e):
     self.Close()
 
+  def OnDelete(self, e):
+    profiles = self.controller.GetProfiles()
+    stdmsg = "Wählen sie das Profil, das gelöscht werden soll"
+    stdcap = "Profil löschen"
+    stddlg = wx.SingleChoiceDialog(self, stdmsg, stdcap, profiles)
+        
+    if stddlg.ShowModal() == wx.ID_OK:
+      select = profiles[stddlg.GetSelection()]
+      self.controller.Delete(select)
+
   def OnStandard(self, e):
-    pass
+    profiles = ['Keines'] +  self.controller.GetProfiles()
+    stdmsg = "Wählen sie das Standardprofil, das beim Anmelden geladen wird:"
+    stdcap = "Standardprofil wählen"
+    stddlg = wx.SingleChoiceDialog(self, stdmsg, stdcap, profiles)
+        
+    if stddlg.ShowModal() == wx.ID_OK:
+      select = profiles[stddlg.GetSelection()]
+      if select == "Keines":
+        select = None
+      self.controller.SetStandard(select)
     
   def OnApply(self, e):
-    self.controller.Apply(e.GetEventObject().GetName())
+    self.controller.SetProfile(e.GetEventObject().GetName())
 
   def OnSave(self, e):
     pass
@@ -108,7 +138,7 @@ def main():
   frame.AddEntry(name="Blah", comment="Das ist ein langer, mehrzeiliger Kommentar. Ja \
 wirklich! Lorem ipsum und so.")
   frame.AddEntry(name="Blubb", comment="Das ist ein langer, mehrzeiliger Kommentar. Ja \
-wirklich! Lorem ipsum und so.", status=["standard", "detected"])
+wirklich! Lorem ipsum und so.", status=["standard", "detected", "active"])
   frame.Fit()
   frame.CenterOnScreen()
   frame.Show(True)
