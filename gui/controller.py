e84cac3218028f94ba24ccf368a1b8d1b3a870b0
Georg Vogetseder <Georg Vogetseder externer.dl.vogetseder@muenchen.de>
2012-01-19 17:44:37 +0100

    Add standard and detected info to listing. Remove Entries before Drawing GUI.

--- gui/controller.py
+++ gui/controller.py
@@ -26,15 +26,28 @@ class Controller:
 
   def SetStandard(self, name):
     self.autorandr.setdefaultprofile(name)
+    self.ListProfilesGUI()
+
+  def Delete(self, name):
+    print "I would delete {0}".format(name)
 
   def ListProfilesGUI(self):
     profiles = self.autorandr.getprofiles()
+    for i in self.gui.vertbox.GetChildren():
+      i.DeleteWindows()
+      del i
     for i in profiles:
       info = self.autorandr.getprofileinfo(i)
+      status = []
+      if info['name'] == self.autorandr.getdefaultprofile():
+        status = ['standard']
+      if info['name'] in self.autorandr.getdetectedprofile():
+        status = status + ['detected']
       try:
-        self.gui.AddEntry(name=info['name'], comment=info['comment'])
+        self.gui.AddEntry(name=info['name'], comment=info['comment'], \
+            status=status)
       except KeyError as e:
-        self.gui.AddEntry(name=info['name'])
+        self.gui.AddEntry(name=info['name'], status=status)
     self.gui.drawme()
 
 
