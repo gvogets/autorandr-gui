fade3e73841d6198514e222a4fc25cdad80605ce
Georg Vogetseder <Georg Vogetseder externer.dl.vogetseder@muenchen.de>
2012-04-03 16:01:44 +0200

    Add disper support to launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -3,6 +3,11 @@
 if [  "$1" = "nvidia" ]; then
   [ -x /usr/bin/nvidia-settings ] && \
     /usr/bin/nvidia-settings -p "X Server Display Configuration"
+elif [ "$1" = "disper" ]; then
+  if [ -x /usr/bin/disper ]; then
+    shift
+    /usr/bin/disper $@
+  fi
 else
   [ -x /usr/bin/arandr ] && \
     /usr/bin/arandr
