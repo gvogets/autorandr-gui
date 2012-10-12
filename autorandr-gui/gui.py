b7f39cc9554763d5a30615c21c749a9e585ffa94
Georg Vogetseder <externer.dl.vogetseder@muenchen.de>
2012-10-12 13:23:35 +0200

    Add License Headers

--- autorandr-gui/gui.py
+++ autorandr-gui/gui.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
-# coding=utf-8
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2012 Landeshauptstadt München
+# All rights reserved.
+#
+# Licensed under the EUPL, Version 1.0 only (the "Licence");
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at:
+#
+# http://joinup.ec.europa.eu/software/page/eupl
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the Licence is distributed on an "AS IS" basis, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the Licence for the specific language governing permissions and
+# limitations under the Licence.
 
 import wx
 import xdg.IconTheme
